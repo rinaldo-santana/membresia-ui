d128ee50572a2e99178a35c45b9c73c45d5b35a6
RinaldoSantana <rinaldo.rms@gmail.com>
2018-09-23 21:23:28 -0300

    correção da porta no server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.all('*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/membresia-ui/index.html'));
 });
 
-const port = (process.env.PORT || 4300);
-app.listen(port, () => {
+const porta = (process.env.PORT || 4300);
+app.listen(porta, () => {
 	console.info(`Aplicacao membresia-ui rodando na porta ${porta}`);
 });
